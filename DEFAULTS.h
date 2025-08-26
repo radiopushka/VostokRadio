dc9798880386b5ddb77959d488594f4be8134885
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-08-26 13:04:52 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -2,17 +2,17 @@
 #define DEFAULTS
 
 //default frequency muxer:
-int fdef[]={190,500,3000,6000,15000};
+int fdef[]={190,500,3000,7000,15000};
 int fdef_size=5;
 
-float def_attack[]={0.0000000001      ,0.000001     ,0.001     ,0.001     ,0.01};
-float def_release[]={0.00000000000001  ,0.000000001  ,0.00003   ,0.00003   ,0.001};
-float def_target[]={15000,17000,18000,19000,22000};
+float def_attack[]={0.0000000001      ,0.000001     ,0.0001     ,0.0002     ,0.01};
+float def_release[]={0.00000000000001  ,0.000000001  ,0.000003   ,0.000003   ,0.005};
+float def_target[]={15000,17000,17000,17000,24000};
 float def_m_gain[]={400,400,400,400,400};
-float pre_amp[]={1,2,1,4,18};
+float pre_amp[]={1,2,1,4,40};
 float def_gate[]={6000,3000,4000,4000,4000};
 int bypass[]={0,0,0,0,0};
-float post_amp[]={1,1,1,1,3};
+float post_amp[]={1,1,1,1,1};
 int types[]={COMP_RMS,COMP_RMS,COMP_PEAK,COMP_PEAK,COMP_PEAK};
 
 #define FINAL_AMP 1
@@ -28,9 +28,9 @@ int types[]={COMP_RMS,COMP_RMS,COMP_PEAK,COMP_PEAK,COMP_PEAK};
 #define STEREO 1
 #define STEREO_GAIN 1.6
 
-#define AGC_TARG 15000
+#define AGC_TARG 12000
 
-#define AGC_SPEED 0.0000000000000001
+#define AGC_SPEED 0.0001
 #define AGC_GATE 3000
 
 // set to 1 to show the levels in real time, 0 to keep silent
