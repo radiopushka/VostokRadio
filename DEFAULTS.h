4c3c1f1c286f0444286e7f3ae7b9ab9f54c8ebe9
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-09-23 20:54:50 -0700

    Update DEFAULTS.h

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -3,15 +3,15 @@
 
 //Evan Nikitin 2025
 
-int fdef[]={60,250,500,2000,6000,16000}; //multiband compression filters
+int fdef[]={60,250,500,3000,6000,16000}; //multiband compression filters
 int fdef_size=6;
 
 float mix_stereo[]={0           ,0              ,0          ,0 ,0         ,0     };
-float def_attack[]={ 0.100   ,0.005  ,0.001 ,0.001  ,0.001   ,0.020};//multiband compression attack
-float def_release[]={ 0.500  ,0.200  ,0.850 ,0.850  ,0.500   ,0.050}; //multiband compression release
-float def_target[]={6000,5000,5000,5000,8000,18000}; //multiband compression target volume 
+float def_attack[]={ 0.100   ,0.005  ,0.001 ,0.001  ,0.001   ,0.001};//multiband compression attack
+float def_release[]={ 0.500  ,0.200  ,0.850 ,0.850  ,0.500   ,0.010}; //multiband compression release
+float def_target[]={12000,5000,5000,5000,8000,18000}; //multiband compression target volume 
 float def_m_gain[]={1.6,1.5,1.1,1.1,1.5,2}; //make up gain
-float pre_amp[]={6,1,0.4,0.4,1,20}; //multiband compressor pre compression gain
+float pre_amp[]={12,1,0.4,0.4,1,20}; //multiband compressor pre compression gain
 float def_gate[]={0,0,0,0,0,0}; //multi band compressor gate
 int bypass[]={0,0,0,0,0,0}; //band compression bypass
 float post_amp[]={1,1,1,1,1,1}; // band compression post amplification
@@ -37,15 +37,15 @@ int types[]={COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS};//band compr
 #define GATE_THRESHOLD 2000
 */
 
-#define FINAL_AMP 2 // can change the global gain after the multiband compressor
+#define FINAL_AMP 1 // can change the global gain after the multiband compressor
 #define FINAL_CLIP//comment to disable and use a gain leveler instead(not recommended)
 //#define FINAL_CLIP_LOOKAHEAD 100 //samples
 //#define FINAL_CLIP_LOOKAHEAD_RELEASE 0.004 //release coeficient, proportional to # samples
 #define SIGMOIDAL_CO 5
-#define SIGMOIDAL_ATTACK  0.1
-#define SIGMOIDAL_RELEASE 0.01
+#define SIGMOIDAL_ATTACK  0.002
+#define SIGMOIDAL_RELEASE 0.0002
 #define SIGMOIDAL_BUFFER 50
-#define SIGMOIDAL_DRANGE 6000 //this should be near the start of the convergance to 1 or -1 of the tanh function relative to the limit
+#define SIGMOIDAL_DRANGE 9000 //this should be near the start of the convergance to 1 or -1 of the tanh function relative to the limit
 //Vostok RF AM transmitters can handle low bass pretty well, you could set this to 20hz
 //most other AM transmitters require bass cut, so set this to like 70hz
 //some PLLVCO based FM transmitters might also require bass cut, our current model has trouble with bass.
@@ -53,7 +53,7 @@ int types[]={COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS};//band compr
 //The PLL VCO removes this noise due to its' feedback loop mechanism but this then re generates that noise waveform at the varactor diode and your audio signal will be out of phase with it.
 
 #define HIGH_PASS // for FM transmitters that have trouble with low frequency bass
-#define HIGH_PASS_CUTOFF 25 //comment the line above to disable
+#define HIGH_PASS_CUTOFF 40 //comment the line above to disable
 #define DC_REMOVAL_COEFF 0.005
 
 
