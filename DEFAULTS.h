30f613c824df0a12def6d4670eb2071b420bf981
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-09-25 00:07:29 -0700

    cleaner highs

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -7,8 +7,8 @@ int fdef[]={60,250,350,3000,6000,16000}; //multiband compression filters
 int fdef_size=6;
 
 float mix_stereo[]={0           ,0              ,0          ,0 ,0         ,0     };
-float def_attack[]={ 0.070   ,0.0005  ,0.0008 ,0.0011  ,0.0015   ,0.0010};//multiband compression attack
-float def_release[]={ 0.500  ,0.0600  ,0.0650 ,0.0500  ,0.0100   ,0.1000}; //multiband compression release
+float def_attack[]={ 0.070   ,0.0005  ,0.0008 ,0.0011  ,0.0015   ,0.00000010};//multiband compression attack
+float def_release[]={ 0.500  ,0.0600  ,0.0650 ,0.0500  ,0.0100   ,0.00001000}; //multiband compression release
 float def_target[]={5000,4000,4000,4000,5000,5000}; //multiband compression target volume 
 float def_m_gain[]={1.6,1.5,1.1,1.1,1.1,2}; //make up gain
 float pre_amp[]={8,1,0.4,0.1,0.2,20}; //multiband compressor pre compression gain
