e620572e86fffbad06d8a35be583891022820c82
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-09-25 13:11:27 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -7,8 +7,8 @@ int fdef[]={60,250,350,3000,6000,16000}; //multiband compression filters
 int fdef_size=6;
 
 float mix_stereo[]={0           ,0              ,0          ,0 ,0         ,0     };
-float def_attack[]={ 0.070   ,0.00001  ,0.00002 ,0.000011  ,0.000015   ,0.000000010};//multiband compression attack
-float def_release[]={ 0.500  ,0.800  ,0.850 ,0.800  ,0.400   ,0.000001000}; //multiband compression release
+float def_attack[]={ 0.140   ,0.00001  ,0.00002 ,0.000011  ,0.000015   ,0.000000010};//multiband compression attack
+float def_release[]={ 1.000  ,0.800  ,0.850 ,0.800  ,0.400   ,0.000001000}; //multiband compression release
 float def_target[]={5000,4000,4000,4000,5000,5000}; //multiband compression target volume 
 float def_m_gain[]={1.6,1.5,1.1,1.1,1.1,2}; //make up gain
 float pre_amp[]={8,1,0.1,0.2,0.3,20}; //multiband compressor pre compression gain
