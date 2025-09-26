3bdd5b72dd465c8f85d09e1aadf5f5e71e78af58
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-09-25 23:12:36 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -7,11 +7,11 @@ int fdef[]={60,250,350,3000,6000,16000}; //multiband compression filters
 int fdef_size=6;
 
 float mix_stereo[]={0           ,0              ,0          ,0 ,0         ,0     };
-float def_attack[]={ 0.340   ,0.01  ,  0.04    ,0.071  , 0.035   ,0.005};//multiband compression attack
+float def_attack[]={ 0.140   ,0.01  ,  0.04    ,0.071  , 0.035   ,0.0005};//multiband compression attack
 float def_release[]={ 1.000 , 0.1700  ,0.6500 ,0.3500  ,0.1000   ,0.01000}; //multiband compression release
-float def_target[]={22000,20000,21000,21000,23000,24000}; //multiband compression target volume 
+float def_target[]={20000,20000,21000,21000,23000,24000}; //multiband compression target volume 
 float def_m_gain[]={1.3,1.3,1.15,1.15,1.6,2}; //make up gain
-float pre_amp[]={0.7,1,0.1,0.2,0.3,1}; //multiband compressor pre compression gain
+float pre_amp[]={0.8,1,0.2,0.4,0.6,1}; //multiband compressor pre compression gain
 float def_gate[]={0,0,0,0,0,0}; //multi band compressor gate
 int bypass[]={0,0,0,0,0,0}; //band compression bypass
 float post_amp[]={1,0.05,0.1,0.2,1,1}; // band compression post amplification
@@ -77,10 +77,10 @@ int types[]={COMP_PEAK,COMP_RMS,COMP_RMS,COMP_RMS,COMP_PEAK,COMP_PEAK};//band co
 
 #define AGC_TARG 20000 //input AGC baseline target
 
-#define AGC_SPEED 0.002 //response coefficient
+#define AGC_SPEED 0.00002 //response coefficient
 //#define AGC_SPEED 0 //response coefficient
 //#define AGC_RELEASE 0.0000000001 //response coefficient
-#define AGC_RELEASE 0.002 //response coefficient
+#define AGC_RELEASE 0.00002 //response coefficient
 #define AGC_GATE 15
 
 // set to 1 to show the levels in real time, 0 to keep silent
