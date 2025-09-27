4cedb6d14fe9370789f8d8c2bf8a4c7b8e480e8a
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-09-27 00:33:21 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -9,16 +9,16 @@ int fdef_size=6;
 
 float mix_stereo[]={0           ,0              ,0          ,0 ,0         ,0     };
 int   lookaheads[]={40 ,  40 , 40, 40 ,40, 40}; // will affect phase
-float def_attack[]={ 20   ,5  ,  3  ,1  , 0.5  ,4};//multiband compression attack
-float def_release[]={ 600 , 300  ,100 ,50 ,20   ,16}; //multiband compression release
-float def_target[]={26000,20000,20000,20000,20000,27000}; //multiband compression target volume 
-float def_m_gain[]={1.3,1,1.2,1.3,1.5,1}; //make up gain
-float pre_amp[]={4,1,1,1,1,40}; //multiband compressor pre compression gain
+float def_attack[]={ 20   ,5  ,  3  ,1  , 1  ,6};//multiband compression attack
+float def_release[]={ 600 , 300  ,100 ,50 ,50   ,16}; //multiband compression release
+float def_target[]={26000,20000,20000,20000,20000,54000}; //multiband compression target volume 
+float def_m_gain[]={1.3,1,1.2,1.3,1.5,1.1}; //make up gain
+float pre_amp[]={4,1,1,1,1,80}; //multiband compressor pre compression gain
 float def_gate[]={0,0,0,0,0,0}; //multi band compressor gate
 int bypass[]={0,0,0,0,0,0}; //band compression bypass
-float post_amp[]={1,1,1,1,1,2}; // band compression post amplification
+float post_amp[]={1,1,1,1,1,1}; // band compression post amplification
 float effect[]={4,6,4.0,4.0,4.0,8.0};//ratio
-float knee[]={20000,15000,8000,4000,2000,10};//knee
+float knee[]={20000,15000,8000,4000,2000,200};//knee
 int types[]={COMP_PEAK,COMP_RMS,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK};//band compression compressor types
                   
 //this maximizes loudness, you can comment this out if you are using mono
