10b11ce888321c1994a6fbb844dd03ddf2f90919
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-09-28 00:17:45 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -18,7 +18,7 @@ float def_gate[]={0,0,0,0,0,0}; //multi band compressor gate
 int bypass[]={0,0,0,0,0,0}; //band compression bypass
 float post_amp[]={1,1,1,1,1,1}; // band compression post amplification
 float effect[]={4,6,6.0,8.0,9.0,12.0};//ratio
-float knee[]={10000,8000,8000,8000,8000,20};//knee
+float knee[]={10000,8000,8000,8000,8000,1000};//knee
 int types[]={COMP_PEAK,COMP_RMS,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK};//band compression compressor types
                   
 //this maximizes loudness, you can comment this out if you are using mono
