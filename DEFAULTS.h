86ba3b008c513b90b1e43d0c53fd4042233cc482
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-09-28 15:08:32 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -77,7 +77,7 @@ int types[]={COMP_PEAK,COMP_RMS,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK};//band 
 			 
 #define POST_AGC_GAIN 1
 
-#define AGC_TARG 11000 //input AGC baseline target
+#define AGC_TARG 9000 //input AGC baseline target
 #define AGC_LOOKAHEAD 1000
 #define AGC_SPEED 0.00003 //response coefficient
 //#define AGC_SPEED 0 //response coefficient
