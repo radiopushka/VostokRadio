e2c9a557aeee6b823211bb35a216877746a22cba
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-10-01 02:14:23 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -43,7 +43,7 @@ int types[]={COMP_PEAK,COMP_RMS,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK};//band 
 #define GATE_ATTACK 0.001
 #define GATE_THRESHOLD 2000
 */
-#define PRE_CLIP_SATURATION 0.85
+#define PRE_CLIP_SATURATION 0.9
 #define PRE_CLIP_SATURATION_LIMIT 0.7
 #define POST_SAT_GAIN 1.6
 
@@ -56,7 +56,7 @@ int types[]={COMP_PEAK,COMP_RMS,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK};//band 
 #define SIGMOIDAL_RELEASE 4
 #define SIGMOIDAL_BUFFER 100
 #define SIGMOIDAL_KNEE 25
-#define SIGMOIDAL_DRANGE -27000 //this should be near the start of the convergance to 1 or -1 of the tanh function relative to the limit
+#define SIGMOIDAL_DRANGE -23000 //this should be near the start of the convergance to 1 or -1 of the tanh function relative to the limit
 //Vostok RF AM transmitters can handle low bass pretty well, you could set this to 20hz
 //most other AM transmitters require bass cut, so set this to like 70hz
 //some PLLVCO based FM transmitters might also require bass cut, our current model has trouble with bass.
