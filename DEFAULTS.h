d366eb7d57351800ef5c7d99e234a8f9b5b935a6
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-10-25 03:18:54 -0700

    more semetric limiter setting

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -62,7 +62,7 @@ int types[]={COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK};//band
 #define SIGMOIDAL_ATTACK  200
 #define SIGMOIDAL_RELEASE 200
 #define SIGMOIDAL_BUFFER 3
-#define SIGMOIDAL_KNEE 7000
+#define SIGMOIDAL_KNEE 1000
 #define SIGMOIDAL_DRANGE 7000 //this should be near the start of the convergance to 1 or -1 of the tanh function relative to the limit
 //Vostok RF AM transmitters can handle low bass pretty well, you could set this to 20hz
 //most other AM transmitters require bass cut, so set this to like 70hz
