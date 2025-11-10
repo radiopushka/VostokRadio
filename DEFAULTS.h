a651a2b45d244edf2ace0da22baa67349fb9d885
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-11-09 18:45:29 -0700

    complete bass distortion removal

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -18,7 +18,7 @@ float def_gate[]={3000,3000,3000,3000,3000,5000,5000}; //multi band compressor g
 int bypass[]={0,0,0,0,0,0,0}; //band compression bypass
 float post_amp[]={1,1,1,1,1,1.2,1}; // band compression post amplification
 float effect[]={12,12,7,8,4,4,6};//ratio
-float knee[]={2,1,3,2,2,5,2};//knee
+float knee[]={2,1,1,2,2,5,2};//knee
 float knee_release[]={2,1.2,2,0.8,0.8,2,0.8};//knee
 //int types[]={COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK,COMP_PEAK};//band compression compressor types
 
