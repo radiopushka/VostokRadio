67c062fd497a823be2a45c76d93c4bd862836111
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-12-10 17:39:06 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -64,7 +64,7 @@ float knee_release[]={1.005,1.005,1,1,1.6,1.6,1};//knee
 #define SIGMOIDAL_CO 2
 #define SIGMOIDAL_ATTACK  0.2667
 #define SIGMOIDAL_RELEASE 0.6665
-#define SIGMOIDAL_BUFFER 200
+#define SIGMOIDAL_BUFFER 600
 #define SIGMOIDAL_KNEE 4800
 #define SIGMOIDAL_DRANGE -520000 //this should be near the start of the convergance to 1 or -1 of the tanh function relative to the limit
 //Vostok RF AM transmitters can handle low bass pretty well, you could set this to 20hz
