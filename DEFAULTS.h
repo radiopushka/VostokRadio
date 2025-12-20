5c5e26b050287d8869cb1bfa34aebfb005b7bde5
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-12-20 12:17:24 -0700

    Add files via upload

--- DEFAULTS.h
+++ DEFAULTS.h
@@ -109,8 +109,8 @@ float knee_release[]={1.002,1.001,1,1,1,1,1};//knee
 #define COMPOSITE_CLIPPER_LOOKAHEAD 20
 #define COMPOSITE_CLIPPER_LOOKAHEAD_RELEASE 0.00006
 #define PERCENT_PILOT 0.12 //percent of the signal devoted to the 19khz pilot tone
-#define PERCENT_MONO 8 // percent of the signal devoted to mono audio
-#define PERCENT_STEREO 8// percent of the signal devoted to mono audio
+#define PERCENT_MONO 7 // percent of the signal devoted to mono audio
+#define PERCENT_STEREO 7// percent of the signal devoted to mono audio
 			   // sometimes if there is distortion, decreasing the percent stereo could help
 
 
